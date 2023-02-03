8ba1edb1ecd310a6fa337af1e743fd6d5ed092c4
TatianaChepovaya <taniachepovaya@gmail.com>
2023-02-03 18:56:21 +0200

    .

---
+++ menu-mob.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  //   console.log("ready!");
+  $(".mobile__menu-icon").on("click", function () {
+    //  alert();
+    $(".mobile__menu-container").toggleClass("active");
+  });
+});
